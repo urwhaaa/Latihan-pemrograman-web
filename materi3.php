a85d525822e255645dabf1b95e249b718080069c
urwhaaa <urwathulwustha@gmail.com>
2026-04-22 10:43:30 +0800

    Update materi3.php

--- materi3.php
+++ materi3.php
@@ -1,56 +1,24 @@
 <?php
-echo "<h2>Materi 3: Perulangan & Function</h2>";
-
-/* ================= WHILE ================= */
-echo "<h3>1. While</h3>";
-echo "Tampilkan angka dari 1 sampai 5 menggunakan while <br>";
-$i = 1;
-while ($i <= 5) {
-    echo $i . " ";
-    $i++;
+echo "<h3>1. Function tanpa parameter</h3>";
+function sapa() {
+    echo "Halo!";
 }
 
-echo "<br><br>";
+sapa();
 
-/* ================= DO WHILE ================= */
-echo "<h3>2. Do While</h3>";
-echo "Tampilkan angka dari 1 sampai 5 menggunakan do while <br>";
-
-$i = 1;
-do {
-    echo $i . " ";
-    $i++;
-} while ($i <= 5);
-
-echo "<br><br>";
-
-/* ================= FOR ================= */
-echo "<h3>3. For</h3>";
-echo "Tampilkan angka dari 1 sampai 5 menggunakan for <br>";
-
-for ($i = 1; $i <= 5; $i++) {
-    echo $i . " ";
+echo "<h3>2. Function dengan parameter</h3>";
+function sapaNama($nama) {
+    echo "Halo, $nama!";
 }
 
-echo "<br><br>";
+sapaNama("Urwhaaa");
 
-/* ================= FOR INCREMENT ================= */
-echo "<h3>4. For Increment (Loncat 2)</h3>";
-echo "Tampilkan angka ganjil dari 1 sampai 10 <br>";
-
-for ($i = 1; $i <= 10; $i += 2) {
-    echo $i . " ";
+echo "<h3>3. Function dengan return</h3>";
+function tambah($a, $b) {
+    return $a + $b;
 }
 
-echo "<br><br>";
+$hasil = tambah(5, 7);
+echo $hasil;
 
-/* ================= FOR DECREMENT ================= */
-echo "<h3>5. For Mundur</h3>";
-echo "Tampilkan angka dari 10 sampai 1 <br>";
-
-for ($i = 10; $i >= 1; $i--) {
-    echo $i . " ";
-}
-
-echo "<br>";
 ?>
